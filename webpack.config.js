30e573f2404defa92354c96fe7ef99eb0ae86380
citizenlab <jeremie.cyrille@icloud.com>
2021-06-21 17:22:02 +0200

    add - scalefolding

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ Encore
     .copyFiles({
         from: './assets/images',
         // optional target path, relative to the output dir
-        //to: 'images/[path][name].[ext]',
+        to: 'images/[name].[ext]',
         // if versioning is enabled, add the file hash too
         //to: 'images/[path][name].[hash:8].[ext]',  
         // only copy files matching this pattern
